0d5e10a9ea917de63a53fff64f7eed0a4360fd50
sahil_sawhney <sahilsawhney55@hotmail.com>
2018-05-19 05:43:19 +0530

    Dummy project

---
+++ src/main/scala/com/madhukaraphatak/akkahttp/AkkaJsonParsing.scala
@@ -0,0 +1,51 @@
+package com.madhukaraphatak.akkahttp
+
+import java.util.concurrent.ConcurrentLinkedDeque
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import com.madhukaraphatak.akkahttp.Models.{Customer, ServiceJsonProtoocol}
+import spray.json.DefaultJsonProtocol._
+import scala.collection.JavaConverters._
+
+/**
+  * Created by madhu on 8/11/15.
+  */
+object AkkaJsonParsing {
+
+
+
+  def main(args: Array[String]) {
+
+    implicit val actorSystem = ActorSystem("rest-api")
+
+    implicit val actorMaterializer = ActorMaterializer()
+
+    val list = new ConcurrentLinkedDeque[Customer]()
+
+    import ServiceJsonProtoocol.customerProtocol
+    val route =
+      path("customer") {
+        post {
+          entity(as[Customer]) {
+            customer => complete {
+              list.add(customer)
+              s"got customer with name ${customer.name}"
+            }
+          }
+        } ~
+          get {
+            complete {
+              list.asScala
+            }
+          }
+      }
+
+    Http().bindAndHandle(route, "0.0.0.0", 8080)
+  }
+
+
+}
